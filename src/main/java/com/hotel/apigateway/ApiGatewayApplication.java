577fb414400234b97b9090f0c62abac4f1c01904
Rupesh <rupesh16495@gmail.com>
2024-05-07 08:32:12 -0700

    routes added

--- src/main/java/com/hotel/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/hotel/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,28 @@ package com.hotel.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("customer-service", r -> r
+                        .path("/customers/**")
+                        .uri("lb://CUSTOMER-SERVICE")
+                ).route("hotel-service", r -> r
+                        .path("/hotels/**")
+                        .uri("lb://HOTEL-MANAGEMENT-SERVICE"))
+                .build();
+    }
 }
